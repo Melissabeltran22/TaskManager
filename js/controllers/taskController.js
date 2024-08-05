80500be0aae23c0c7b7b3800be891823628e7b08
Melissa Beltran <meli.cesde@gmail.com>
2024-08-05 18:09:17 -0500

    Se crean los controladres para maanejar el registro e inicio de sesion

---
+++ js/controllers/taskController.js
@@ -0,0 +1,32 @@
+function displayTasks() {
+    const taskList = document.getElementById('taskList');
+    taskList.innerHTML = '';
+  
+    tasks.forEach(task => {
+      const listItem = document.createElement('li');
+      listItem.textContent = `${task.name} - ${task.description} - ${task.deadline} - ${task.status}`;
+      taskList.appendChild(listItem);
+    });
+  }
+  document.addEventListener('DOMContentLoaded', () => {
+    document.addEventListener('submit', function(event) {
+      if (event.target && event.target.id === 'taskForm') {
+        event.preventDefault();
+  
+        const name = document.getElementById('taskName').value;
+        const description = document.getElementById('taskDescription').value;
+        const deadline = document.getElementById('taskDeadline').value;
+        const status = document.getElementById('taskStatus').value;
+  
+        if (name && description && deadline && status) {
+          const newTask = new Task(name, description, deadline, status);
+          tasks.push(newTask);
+          alert('Tarea agregada exitosamente');
+          loadDashboardView();
+        } else {
+          alert('Por favor, complete todos los campos.');
+        }
+      }
+    });
+  });
+  
