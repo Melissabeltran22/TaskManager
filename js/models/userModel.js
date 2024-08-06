69b8fa554f45a7e9fe3b3e416cf4360526fa6746
Melissa Beltran <meli.cesde@gmail.com>
2024-08-06 18:45:43 -0500

    agrego modelo de usuario

---
+++ js/models/userModel.js
@@ -0,0 +1,11 @@
+class User {
+    constructor(name, email, password, department) {
+      this.name = name;
+      this.email = email;
+      this.password = password;
+      this.department = department;
+    }
+  }
+  
+  export default User;
+  
