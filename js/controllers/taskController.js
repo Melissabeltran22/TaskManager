ab9353f5e07f9fd66a51eecccaffd10d882e700c
Melissa Beltran <meli.cesde@gmail.com>
2024-08-06 18:40:28 -0500

    modifico estructura basica del html

--- js/controllers/taskController.js
+++ js/controllers/taskController.js
@@ -8,25 +8,3 @@ function displayTasks() {
       taskList.appendChild(listItem);
     });
   }
-  document.addEventListener('DOMContentLoaded', () => {
-    document.addEventListener('submit', function(event) {
-      if (event.target && event.target.id === 'taskForm') {
-        event.preventDefault();
-  
-        const name = document.getElementById('taskName').value;
-        const description = document.getElementById('taskDescription').value;
-        const deadline = document.getElementById('taskDeadline').value;
-        const status = document.getElementById('taskStatus').value;
-  
-        if (name && description && deadline && status) {
-          const newTask = new Task(name, description, deadline, status);
-          tasks.push(newTask);
-          alert('Tarea agregada exitosamente');
-          loadDashboardView();
-        } else {
-          alert('Por favor, complete todos los campos.');
-        }
-      }
-    });
-  });
-  
---
+++ js/views/taskFormView.js
@@ -0,0 +1,34 @@
+const taskFormView = `
+  <div class="card shadow-sm">
+    <div class="card-body">
+      <h2 class="card-title text-center text-primary">Agregar Nueva Tarea</h2>
+      <form id="taskForm">
+        <div class="form-group">
+          <label for="taskName" class="text-primary">Nombre de la Tarea:</label>
+          <input type="text" class="form-control" id="taskName" required>
+        </div>
+        <div class="form-group">
+          <label for="taskDescription" class="text-primary">Descripción:</label>
+          <textarea class="form-control" id="taskDescription" required></textarea>
+        </div>
+        <div class="form-group">
+          <label for="taskDeadline" class="text-primary">Fecha Límite:</label>
+          <input type="date" class="form-control" id="taskDeadline" required>
+        </div>
+        <div class="form-group">
+          <label for="taskStatus" class="text-primary">Estado:</label>
+          <select class="form-control" id="taskStatus">
+            <option>Pendiente</option>
+            <option>En Progreso</option>
+            <option>Completada</option>
+          </select>
+        </div>
+        <button type="submit" class="btn btn-primary btn-block">Agregar Tarea</button>
+      </form>
+    </div>
+  </div>
+`;
+
+function loadTaskFormView() {
+  document.getElementById('app').innerHTML = taskFormView;
+}
