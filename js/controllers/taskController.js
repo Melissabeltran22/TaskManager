6295b5d51823a72e5d1bab720db58b8f236704e9
Melissa Beltran <meli.cesde@gmail.com>
2024-08-07 19:11:59 -0500

    controlador de task

--- js/controllers/taskController.js
+++ js/controllers/taskController.js
@@ -1,10 +1,22 @@
-function displayTasks() {
-    const taskList = document.getElementById('taskList');
-    taskList.innerHTML = '';
-  
-    tasks.forEach(task => {
-      const listItem = document.createElement('li');
-      listItem.textContent = `${task.name} - ${task.description} - ${task.deadline} - ${task.status}`;
-      taskList.appendChild(listItem);
-    });
-  }
+import Task from '../models/taskModel.js';
+
+function addTask(title, description, dueDate, status, userEmail) {
+  const task = new Task(title, description, dueDate, status, userEmail);
+  const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  tasks.push(task);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function getTasks(userEmail) {
+  const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  return tasks.filter(task => task.userEmail === userEmail);
+}
+
+function deleteTask(taskIndex, userEmail) {
+  const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  const userTasks = tasks.filter(task => task.userEmail === userEmail);
+  userTasks.splice(taskIndex, 1);
+  localStorage.setItem('tasks', JSON.stringify(userTasks));
+}
+
+export { addTask, getTasks, deleteTask };
