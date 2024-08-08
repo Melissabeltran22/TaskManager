2e829798b4590569009c4764a66f7b989c189d95
Melissa Beltran <meli.cesde@gmail.com>
2024-08-07 19:06:52 -0500

    modelo task

---
+++ js/models/taskModel.js
@@ -0,0 +1,11 @@
+class Task {
+    constructor(title, description, dueDate, status, userEmail) {
+      this.title = title;
+      this.description = description;
+      this.dueDate = dueDate;
+      this.status = status;
+      this.userEmail = userEmail;
+    }
+  }
+  
+  export default Task;
